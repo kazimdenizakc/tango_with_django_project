5070ec83e454fae762dbe7fb1d5c981c850c067e
kazimdenizakc <156232134+kazimdenizakc@users.noreply.github.com>
2025-01-24 22:21:26 +0000

    Update views.py

--- rango/views.py
+++ rango/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 def index(request):
   return HttpResponse('Rango says hey there partner! <a href="/rango/about/">About</a>')
 def about(request):
-  return HttpResponse("Rango says here is the about page.")
+  return HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>')
 
