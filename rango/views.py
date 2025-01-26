eda9f762c5684859fd61b7b41e30d46b31a6cd5f
kazimdenizakc <156232134+kazimdenizakc@users.noreply.github.com>
2025-01-26 20:47:00 +0000

    Update views.py

--- rango/views.py
+++ rango/views.py
@@ -11,5 +11,5 @@ def index(request):
   return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-  return render(request, 'rango/about.html', context=context_dict)
+  return render(request, 'rango/about.html')
 
