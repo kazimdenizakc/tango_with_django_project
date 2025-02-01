3e286fd1a32363fb1375654a35ce03abc4bb0d46
kazimdenizakc <156232134+kazimdenizakc@users.noreply.github.com>
2025-02-01 22:20:56 +0000

    Update models.py

--- rango/models.py
+++ rango/models.py
@@ -3,7 +3,9 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     likes= models.IntegerField(default=0)
     views= models.IntegerField(default=0)
-    
+    class Meta:
+        verbose_name_plural = "Categories" 
+
     def __str__(self): 
       return self.name
 class Page(models.Model):
