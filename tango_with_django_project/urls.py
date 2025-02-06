67cd6bf1ab344254d8b8dfa415a915fd22f1eae2
“Kazim <2917569a@student.gla.ac.uk>
2025-02-06 23:43:08 +0000

    makin urls changes

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -22,7 +22,8 @@ from rango import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
-    # The above maps any URLs starting with rango/ to be handled by rango. 
+    path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
