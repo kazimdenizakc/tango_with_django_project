1b7d66b873343cb49ef17696c7b846ed1c3b50dc
“Kazim <2917569a@student.gla.ac.uk>
2025-02-06 23:37:46 +0000

    making changes

--- rango/urls.py
+++ rango/urls.py
@@ -5,5 +5,7 @@ app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('rango/', include('rango.urls')),
     path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
