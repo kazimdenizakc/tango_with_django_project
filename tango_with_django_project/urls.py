5fad94202df1a0a666443853a35cf7615052c90b
“Kazim <2917569a@student.gla.ac.uk>
2025-02-06 23:51:00 +0000

    last change

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rango/', include('rango.urls')),  
+    path('rango/', include('rango.urls')),
+    path('', lambda request: HttpResponseRedirect('/rango/')),  # Redirect root to rango
 ]
