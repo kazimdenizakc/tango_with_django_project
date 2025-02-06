4ed68170c5d0aa7cbb1dab158b96b0b57c9acf44
“Kazim <2917569a@student.gla.ac.uk>
2025-02-06 22:35:43 +0000

    update views

--- rango/views.py
+++ rango/views.py
@@ -5,12 +5,18 @@ from rango.models import Page
 
 
 def index(request):
-  # Construct a dictionary to pass to the template engine as its context.
-  # Note the key boldmessage matches to {{ boldmessage }} in the template! 
+  # Query the top 5 categories by likes
   category_list = Category.objects.order_by('-likes')[:5]
-  context_dict = {}
-  context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-  context_dict['categories'] = category_list
+
+  # Query the top 5 pages by views
+  page_list = Page.objects.order_by('-views')[:5]
+
+  # Create a context dictionary to pass to the template
+  context_dict = {
+      'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+      'categories': category_list,
+      'pages': page_list,
+  }
 
   
   # Return a rendered response to send to the client.
