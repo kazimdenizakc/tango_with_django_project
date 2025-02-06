c8a20592db3b7ae2c17956658e5dc90194743a65
“Kazim <2917569a@student.gla.ac.uk>
2025-02-06 23:45:46 +0000

    url changes

--- rango/urls.py
+++ rango/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path,include
+from django.urls import path
 from rango import views
 
-app_name = 'rango'
+app_name = 'rango'  
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('rango/', include('rango.urls')),
-    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
