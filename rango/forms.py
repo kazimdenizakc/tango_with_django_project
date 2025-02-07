fd415389d0c9a3ab49c92cfe492aef4e43ad8fa6
“Kazim <2917569a@student.gla.ac.uk>
2025-02-07 14:49:56 +0000

    update forms

--- rango/forms.py
+++ rango/forms.py
@@ -9,9 +9,9 @@ class CategoryForm(forms.ModelForm):
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
 
-class Meta:
-    model = Category
-    fields = ('name',)
+    class Meta:
+        model = Category
+        fields = ('name',)
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128,
@@ -19,6 +19,6 @@ class PageForm(forms.ModelForm):
     url = forms.URLField(max_length=200,
                          help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-class Meta:
-    model = Page
-    exclude = ('category',)
+    class Meta:
+        model = Page
+        exclude = ('category',)
