04bcb8f4eb14c312a7015ca0990f772270347ba6
“Kazim <2917569a@student.gla.ac.uk>
2025-02-08 12:54:28 +0000

    update in chapter10

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -114,6 +114,8 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
 ]
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
 
 
 # Internationalization
